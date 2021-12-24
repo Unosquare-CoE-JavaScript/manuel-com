bac4f80ed19a502ac76c6b85ad79f8d16659dc24
Manuel Comi <manuel.comi@unosquare.com>
2021-12-24 12:28:58 -0600

    Finished course

---
+++ Asynchronous JavaScript Patterns/Using_Promises_pt1/js/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+let wordnikWords = 'http://api.wordnik.com/v4/words.json/',
+  wordnikWord = 'http://api.wordnik.com/v4/word.json/',
+  apiKey = '?api_key=2efe06dd56a60633b30010e4d970da03b55279db9896d7127',
+  wordObj;
+
+asyncFunction()
+  .then(function (val) {
+    console.log('Yeah!! ' + val);
+    return asyncFunction2();
+  })
+  .then(function (val) {
+    console.log('Second promise: ' + val);
+  });
+
+fetch(wordnikWords + 'randomWord' + apiKey)
+  .then(function (data) {
+    console.log(data.word);
+    return fetch(wordnikWords + data.word + '/definitions' + apiKey);
+  })
+  .then(function (def) {
+    return def.json();
+  })
+  .then(function (def) {
+    console.log(def);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+
+console.log('This is asyc code.');
