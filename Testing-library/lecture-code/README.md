acc3b09c181866cf7a15a1b739dae5f30ec8586e
Manuel Comi <manuel.comi@unosquare.com>
2021-12-27 09:18:41 -0600

    test and code button initial color, when clicked change to red and revert to blue

---
+++ Testing-library/lecture-code/README.md
@@ -0,0 +1,13 @@
+# Lecture Code
+
+These directories contain files for the code written in the optional React coding lectures.
+
+### To install
+
+Copy the contents of the relevant directory into the `src` directory for your project.
+
+### Resources
+
+[WAI_ARIA Roles definitions]([https://link](https://www.w3.org/TR/wai-aria/#role_definitions))
+
+[Jest DOM]([https://link](https://github.com/testing-library/jest-dom))
---
+++ Testing-library/lecture-code/color-button/03-finding-checkbox-with-label/App.test.js
@@ -0,0 +1,24 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import App from './App';
+import { replaceCamelWithSpaces } from './App';
+
+test('button has correct initial color', () => {
+  render(<App />);
+
+  // find an element with a role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole('button', {
+    name: 'Change to blue',
+  });
+
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+
+  // click button
+  fireEvent.click(colorButton);
+
+  // expect the background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+
+  // expect the button text to be 'Change to MediumVioletRed'
+  expect(colorButton).toHaveTextContent('Change to red');
+});
