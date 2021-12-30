f5fbb0ddd83b3eb56d9a56ba38324c3ece556a17
Manuel Comi <manuel.comi@unosquare.com>
2021-12-30 12:34:44 -0600

    test and code scoop option get data from mock server

---
+++ Testing-library/lecture-code/app/src/pages/entry/Options.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import Row from 'react-bootstrap/Row';
+import ScoopOption from './ScoopOption';
+
+export default function Options({ optionType }) {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((err) => {
+        // TODO
+      });
+  }, [optionType]);
+
+  const ItemComponent = optionType === 'scoops' ? ScoopOption : null;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      ket={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+  return <Row>{optionItems}</Row>;
+}
