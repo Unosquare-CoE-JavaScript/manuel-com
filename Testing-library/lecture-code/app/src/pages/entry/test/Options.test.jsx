1bfa6ec3c12ca814b94395ab01c9e8eaafa375fe
Manuel Comi <manuel.comi@unosquare.com>
2021-12-30 13:07:38 -0600

    test and code topping option get data from mock server

--- Testing-library/lecture-code/app/src/pages/entry/test/Options.test.jsx
+++ Testing-library/lecture-code/app/src/pages/entry/test/Options.test.jsx
@@ -13,3 +13,21 @@ test('display images for each scoop option from server', async () => {
   const altText = scoopImages.map((element) => element.alt);
   expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
 });
+
+test('display images for each toppings option from server', async () => {
+  render(<Options optionType='toppings' />);
+
+  // find images
+  const toppingImages = await screen.findAllByRole('img', {
+    name: /topping$/i,
+  });
+  expect(toppingImages).toHaveLength(3);
+
+  // confirm alt text
+  const altText = toppingImages.map((element) => element.alt);
+  expect(altText).toEqual([
+    'Cherries topping',
+    'M&Ms topping',
+    'Hot fudge topping',
+  ]);
+});
