8ddba754e79a11fef7c1ae0c63ab45ad43c37bfb
Manuel Comi <manuel.comi@unosquare.com>
2022-01-03 13:49:37 -0600

    Test and code for alert banners when routes errors

--- Testing-library/lecture-code/app/src/pages/entry/Options.jsx
+++ Testing-library/lecture-code/app/src/pages/entry/Options.jsx
@@ -4,24 +4,28 @@ import axios from 'axios';
 import Row from 'react-bootstrap/Row';
 import ScoopOption from './ScoopOption';
 import ToppingOption from './ToppingOption';
+import AlertBanner from '../common/AlertBanner';
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch((err) => {
-        // TODO
-      });
+      .catch((err) => setError(true));
   }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
 
   const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
 
   const optionItems = items.map((item) => (
     <ItemComponent
-      ket={item.name}
+      key={item.name}
       name={item.name}
       imagePath={item.imagePath}
     />
