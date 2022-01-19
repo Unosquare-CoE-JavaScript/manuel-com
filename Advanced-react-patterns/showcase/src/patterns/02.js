447c0a5f91d229c7d14d323f1181431d743384cd
Manuel Comi <manuel.comi@unosquare.com>
2022-01-18 18:19:01 -0600

    create HOC for clap animation and then transform into a custom hook

--- Advanced-react-patterns/showcase/src/patterns/02.js
+++ Advanced-react-patterns/showcase/src/patterns/02.js
@@ -1,155 +1,137 @@
-import React, { useState, useCallback, useLayoutEffect } from 'react'
+import React, { useState, useLayoutEffect, useCallback } from 'react';
+import mojs from 'mo-js';
+import styles from './index.css';
 
-import mojs from 'mo-js'
-import { generateRandomNumber } from '../utils/generateRandomNumber'
-import styles from './index.css'
+const initialState = {
+  count: 0,
+  countTotal: 267,
+  isClicked: false,
+};
 
-/** ====================================
-   *          🔰Hook
-        Hook for Animation
-  ==================================== **/
-
-const useClapAnimation = ({
-  duration: tlDuration,
-  bounceEl,
-  fadeEl,
-  burstEl
-}) => {
+/**
+ * Custom Hook for animation
+ */
+const useClapAnimation = ({ clapEl, countEl, clapTotalEl }) => {
   const [animationTimeline, setAnimationTimeline] = useState(
-    new mojs.Timeline()
-  )
+    () => new mojs.Timeline()
+  );
 
   useLayoutEffect(() => {
-    if (!bounceEl || !fadeEl || !burstEl) {
-      return
+    if (!clapEl || !countEl || !clapTotalEl) {
+      return;
     }
 
+    const tlDuration = 300;
+    const scaleButton = new mojs.Html({
+      el: clapEl,
+      duration: tlDuration,
+      scale: { 1.3: 1 },
+      easing: mojs.easing.ease.out,
+    });
+
     const triangleBurst = new mojs.Burst({
-      parent: burstEl,
+      parent: clapEl,
       radius: { 50: 95 },
       count: 5,
       angle: 30,
       children: {
         shape: 'polygon',
         radius: { 6: 0 },
-        scale: 1,
-        stroke: 'rgba(211,84,0 ,0.5)',
+        stroke: 'rgba(211,54,0,0.5)',
         strokeWidth: 2,
         angle: 210,
         delay: 30,
         speed: 0.2,
         easing: mojs.easing.bezier(0.1, 1, 0.3, 1),
-        duration: tlDuration
-      }
-    })
+        duration: tlDuration,
+      },
+    });
 
     const circleBurst = new mojs.Burst({
-      parent: burstEl,
+      parent: clapEl,
       radius: { 50: 75 },
       angle: 25,
       duration: tlDuration,
       children: {
         shape: 'circle',
-        fill: 'rgba(149,165,166 ,0.5)',
+        fill: 'rgba(149,165,166,0.5)',
         delay: 30,
         speed: 0.2,
         radius: { 3: 0 },
-        easing: mojs.easing.bezier(0.1, 1, 0.3, 1)
-      }
-    })
+        easing: mojs.easing.bezier(0.1, 1, 0.3, 1),
+      },
+    });
 
     const countAnimation = new mojs.Html({
-      el: bounceEl,
-      isShowStart: false,
-      isShowEnd: true,
-      y: { 0: -30 },
+      el: countEl,
       opacity: { 0: 1 },
-      duration: tlDuration
+      y: { 0: -30 },
+      duration: tlDuration,
     }).then({
       opacity: { 1: 0 },
       y: -80,
-      delay: tlDuration / 2
-    })
+      delay: tlDuration / 2,
+    });
 
     const countTotalAnimation = new mojs.Html({
-      el: fadeEl,
-      isShowStart: false,
-      isShowEnd: true,
+      el: clapTotalEl,
       opacity: { 0: 1 },
       delay: (3 * tlDuration) / 2,
       duration: tlDuration,
-      y: { 0: -3 }
-    })
+      y: { 0: -3 },
+    });
 
-    const scaleButton = new mojs.Html({
-      el: burstEl,
-      duration: tlDuration,
-      scale: { 1.3: 1 },
-      easing: mojs.easing.out
-    })
-
-    if (typeof burstEl === 'string') {
-      clap.style.transform = 'scale(1, 1)'
-      const el = document.getElementById(id)
-      el.style.transform = 'scale(1, 1)'
+    if (typeof clapEl === 'string') {
+      const clap = document.getElementById('clap');
+      clap.style.transform = 'scale(1,1)';
     } else {
-      burstEl.style.transform = 'scale(1, 1)'
+      clapEl.style.transform = 'scale(1,1)';
     }
 
-    const updatedAnimationTimeline = animationTimeline.add([
-      countAnimation,
-      countTotalAnimation,
+    const newAnimationTimeline = animationTimeline.add([
       scaleButton,
+      countTotalAnimation,
+      countAnimation,
+      triangleBurst,
       circleBurst,
-      triangleBurst
-    ])
+    ]);
+    setAnimationTimeline(newAnimationTimeline);
+  }, [clapEl, countEl, clapTotalEl]);
 
-    setAnimationTimeline(updatedAnimationTimeline)
-  }, [tlDuration, animationTimeline, bounceEl, fadeEl, burstEl])
-
-  return animationTimeline
-}
-/** ====================================
-   *      🔰 MediumClap
-  ==================================== **/
-const initialState = {
-  count: 0,
-  countTotal: generateRandomNumber(500, 10000),
-  isClicked: false
-}
+  return animationTimeline;
+};
 
 const MediumClap = () => {
-  const MAXIMUM_USER_CLAP = 50
-  const [clapState, setClapState] = useState(initialState)
-  const { count, countTotal, isClicked } = clapState
+  const MAXIMUM_USER_CLAP = 50;
+  const [clapState, setClapState] = useState(initialState);
+  const { count, countTotal, isClicked } = clapState;
 
-  const [{ clapRef, clapCountRef, clapTotalRef }, setRefState] = useState({})
+  const [{ clapRef, clapCountRef, clapTotalRef }, setRefState] = useState({});
 
-  const setRef = useCallback(node => {
-    if (node !== null) {
-      setRefState(prevRefState => ({
-        ...prevRefState,
-        [node.dataset.refkey]: node
-      }))
-    }
-  }, [])
+  const setRef = useCallback((node) => {
+    setRefState((prevRefState) => ({
+      ...prevRefState,
+      [node.dataset.refkey]: node,
+    }));
+  }, []);
 
   const animationTimeline = useClapAnimation({
-    duration: 300,
-    bounceEl: clapCountRef,
-    fadeEl: clapTotalRef,
-    burstEl: clapRef
-  })
+    clapEl: clapRef,
+    countEl: clapCountRef,
+    clapTotalEl: clapTotalRef,
+  });
 
   const handleClapClick = () => {
-    animationTimeline.replay()
-
-    setClapState({
+    animationTimeline.replay();
+    setClapState((prevState) => ({
+      isClicked: true,
       count: Math.min(count + 1, MAXIMUM_USER_CLAP),
-      countTotal: count < MAXIMUM_USER_CLAP ? countTotal + 1 : countTotal,
-      isClicked: true
-    })
-  }
+      countTotal:
+        count < MAXIMUM_USER_CLAP
+          ? prevState.countTotal + 1
+          : prevState.countTotal,
+    }));
+  };
 
   return (
     <button
@@ -162,19 +144,17 @@ const MediumClap = () => {
       <ClapCount count={count} setRef={setRef} />
       <CountTotal countTotal={countTotal} setRef={setRef} />
     </button>
-  )
-}
+  );
+};
 
-/** ====================================
-   *      🔰SubComponents
-  Smaller Component used by <MediumClap />
-  ==================================== **/
+/**
+ * subcomponents
+ */
 
 const ClapIcon = ({ isClicked }) => {
   return (
     <span>
       <svg
-        id='clapIcon'
         xmlns='http://www.w3.org/2000/svg'
         viewBox='-549 338 100.1 125'
         className={`${styles.icon} ${isClicked && styles.checked}`}
@@ -183,31 +163,30 @@ const ClapIcon = ({ isClicked }) => {
         <path d='M-527.2 399.1l20.9-21.4c2.2 2.2 2.7 2.6 3.5 4.5.8 1.8 1 5.4-1.6 8l-11.8 12.2c-.5.5-.4 1.2 0 1.7.5.5 1.2.5 1.7 0l34-35c1.9-2 5.2-2.1 7.2-.1 2 1.9 2 5.2.1 7.2l-24.7 25.3c-.5.5-.4 1.2 0 1.7.5.5 1.2.5 1.7 0l28.5-29.3c2-2 5.2-2 7.1-.1 2 1.9 2 5.1.1 7.1l-28.5 29.3c-.5.5-.4 1.2 0 1.7.5.5 1.2.4 1.7 0l24.7-25.3c1.9-2 5.1-2.1 7.1-.1 2 1.9 2 5.2.1 7.2l-24.7 25.3c-.5.5-.4 1.2 0 1.7.5.5 1.2.5 1.7 0l14.6-15c2-2 5.2-2 7.2-.1 2 2 2.1 5.2.1 7.2l-27.6 28.4c-11.6 11.9-30.6 12.2-42.5.6-12-11.7-12.2-30.8-.6-42.7m18.1-48.4l-.7 4.9-2.2-4.4m7.6.9l-3.7 3.4 1.2-4.8m5.5 4.7l-4.8 1.6 3.1-3.9' />
       </svg>
     </span>
-  )
-}
+  );
+};
 const ClapCount = ({ count, setRef }) => {
   return (
     <span ref={setRef} data-refkey='clapCountRef' className={styles.count}>
-      +{count}
+      + {count}
     </span>
-  )
-}
+  );
+};
+
 const CountTotal = ({ countTotal, setRef }) => {
   return (
     <span ref={setRef} data-refkey='clapTotalRef' className={styles.total}>
       {countTotal}
     </span>
-  )
-}
+  );
+};
 
-/** ====================================
-      *        🔰USAGE
-      Below's how a potential user
-      may consume the component API
-  ==================================== **/
+/**
+ * Usage
+ */
 
 const Usage = () => {
-  return <MediumClap />
-}
+  return <MediumClap />;
+};
 
-export default Usage
+export default Usage;
